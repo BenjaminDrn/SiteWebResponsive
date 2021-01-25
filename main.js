7f52f68b52244f029dd52aee14dc8d8894779365
BenjaminDrn <drouinbenjamin20@gmail.com>
2021-01-25 22:16:24 +0100

    ajout des différents choix de vêtements et d'images

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 /* ================ MENU SHOW ================ */
 function showMenu(toggleId, navId){
     const toggle = document.getElementById(toggleId),
+    backgroundToggle = document.getElementById('nav-menu')
     nav = document.getElementById(navId)
 
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
+        if(backgroundToggle && nav){
+            backgroundToggle.addEventListener('click', ()=>{
+                nav.classList.toggle('show')
+            })
+        }
     }
+    
 }
 
 showMenu('nav-toggle','nav-menu');
 
+// window loader
